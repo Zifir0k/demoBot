9550a7c113db6f333b4898e25f3cb089060953f4
Zifir0k <evgenizef.ak@gmail.com>
2024-02-20 02:30:15 +1000

    start

---
+++ src/test/java/ai/joy/demoBot/DemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.joy.demoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
