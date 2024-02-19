d271c7d21727bf86f0f528ea6947036e12388456
Zifir0k <evgenizef.ak@gmail.com>
2024-02-20 03:25:02 +1000

    Test

---
+++ src/main/java/ai/joy/demoBot/service/TelegramBot.java
@@ -0,0 +1,63 @@
+package ai.joy.demoBot.service;
+
+import ai.joy.demoBot.config.BotConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final BotConfig botConfig;
+
+    @Autowired
+    public TelegramBot(BotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()){
+            String message = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (message){
+                case "/start":
+                    startCommandReceived(chatId,update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId,"Мне нечего сказать");
+            }
+        }
+
+    }
+    protected void startCommandReceived(long chatId, String name){
+        String answer = "Привет, " + name + " и Добро пожаловать!";
+        sendMessage(chatId,answer);
+    }
+    protected void sendMessage(long chatId, String textToSend){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+}
